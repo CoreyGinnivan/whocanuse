a712960f449eb16f4ae3f5bedbb87eff2ed8c9b6
Corey <corey@ginnivan.net>
2019-09-01 18:53:26 +0800

    Fixed 3 character hex values to switch colour properly

--- src/components/control.js
+++ src/components/control.js
@@ -147,9 +147,16 @@ const SwitchIcon = styled("button")({
 ----------------------------------------------------------*/
 function getTextColor(bgColor, lightColor, darkColor) {
   const color = bgColor.charAt(0) === "#" ? bgColor.substring(1, 7) : bgColor;
-  const r = parseInt(color.substring(0, 2), 16); // hexToR
-  const g = parseInt(color.substring(2, 4), 16); // hexToG
-  const b = parseInt(color.substring(4, 6), 16); // hexToB
+  let r, g, b;
+  if (color.length === 3) {
+    r = parseInt(color.substring(0, 1) + color.substring(0, 1), 16)
+    g = parseInt(color.substring(1, 2) + color.substring(1, 2), 16)
+    b = parseInt(color.substring(2, 3) + color.substring(2, 3), 16)
+  } else {
+    r = parseInt(color.substring(0, 2), 16); // hexToR
+    g = parseInt(color.substring(2, 4), 16); // hexToG
+    b = parseInt(color.substring(4, 6), 16); // hexToB
+  }
   return r * 0.299 + g * 0.587 + b * 0.114 > 186 ? darkColor : lightColor;
 }
 
