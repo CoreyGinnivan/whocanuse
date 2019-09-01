80dee449da3d565431e37c4a194ee124f4189a83
Jake Ginnivan <jake.ginnivan@wanews.com.au>
2019-09-01 18:09:56 +0800

    Fixed lint warning

--- src/components/control.js
+++ src/components/control.js
@@ -1,6 +1,5 @@
-import React, { Component } from "react";
+import React from "react";
 import styled from "@emotion/styled";
-import PropTypes from "prop-types";
 import { SmallText, Text } from "./typography";
 import { theme } from "./theme";
 import Switch from "../images/switch.svg";
