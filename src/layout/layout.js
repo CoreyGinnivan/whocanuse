111f05d4b1bb2e29ba626fc20f090256ad03c3d1
Corey <corey@ginnivan.net>
2019-11-11 16:36:09 +0800

    Added OGP image

--- src/layout/layout.js
+++ src/layout/layout.js
@@ -13,6 +13,7 @@ const Layout = ({ children }) => (
         site {
           siteMetadata {
             title
+            defaultImage: image
           }
         }
       }
@@ -28,7 +29,7 @@ const Layout = ({ children }) => (
           <meta property="og:site_name" content="Who Can Use" />
           <meta property="og:title" content="Find out who can use your color combination" />
           <meta property="og:description" content="An easy-to-use tool that delivers a breakdown of which vision types can see your color combination." />
-          <meta property="og:image" content="http://corey.ginnivan.net/images/home/whocanuse_600.png" />
+          <meta property="og:image" content={image} />
           <meta property="og:url" content="https://www.whocanuse.com/" />
           <meta property="og:image:width" content="600" />
           <meta property="og:image:height" content="600" />
@@ -36,7 +37,7 @@ const Layout = ({ children }) => (
           <meta name="twitter:site" content="@coreyginnivan" />
           <meta name="twitter:title" content="Who Can Use" />
           <meta name="twitter:description" content="Find out who can use your color combination. Whocanuse is an easy-to-use tool that delivers a breakdown of which vision types can see your color combination." />
-          <meta name="twitter:image" content="http://corey.ginnivan.net/images/home/whocanuse_600.png" />
+          <meta name="twitter:image" content={image} />
           <link rel="shortcut icon" type="image/png" href={favicon} />
           <script async defer src="https://buttons.github.io/buttons.js"></script>
         </Helmet>
