c08545f3213d4e93c899b04a42995f9bb9206fcb
Corey <corey@ginnivan.net>
2019-11-11 10:06:24 +0800

    Build error fixes

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,6 +15,14 @@ import { linkPath } from "../helpers/link";
    Styles
 ----------------------------------------------------------*/
 
+// Requiring function causes error during builds
+// as the code tries to reference window
+const module = require("module") // Error
+// Wrap the require in check for window
+if (typeof window !== `undefined`) {
+  const module = require("module")
+}
+
 const MainLayout = styled("div")({
   display: 'flex',
   flexDirection: 'row',
