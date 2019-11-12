933e70f5bb3227f9f8827943bf88a6586588c3d8
Corey <corey@ginnivan.net>
2019-11-12 14:50:50 +0800

    Cleaned up footer copy, added Version tag

--- src/components/about.js
+++ src/components/about.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { Text, MediumText, Heading } from './typography';
+import { theme } from '../components/theme';
 import styled from '@emotion/styled';
 import Avatar from "../images/avatar.png"
 
@@ -18,6 +19,15 @@ const AboutWrapper = styled('div')({
   }
 })
 
+const VersionWrapper = styled('div')({
+  padding: '2px 4px',
+  background: '#f6f8fa',
+  border: `1px solid ${theme.color.grey}`,
+  marginRight: '10px',
+  marginBottom: '4px',
+  borderRadius: '4px',
+  fontSize: '12px'
+})
 
 const Author = styled('div')({
   display: 'flex',
@@ -46,31 +56,32 @@ export class About extends Component {
       <div>
         <AboutWrapper>
           <Heading margin="0 0 20px 0">What is whocanuse.com?</Heading>
-          <Text>Whocanuse.com both an easy to use color contrast tool, and a way to bring attention and understanding to how color contrast affects different users.</Text>
+          <Text>It's a tool that brings attention and understanding to how color contrast can affect different people that have visual impairments.</Text>
 
-          <Text>The WCAG (Web Content Accessbility Guidelines) is a helpful resource for the web, where organisations, companies and goverernments throughout the world came together to agree on how to make the web more accessible. Unfortunately the guidelines and gradings aren't really that easy to understand.</Text>
-          <Text>However rather than giving a generic grade, I thought I'd try and humanise the people who are affeted by a failing grade.</Text>
+          <Text>The <a href="https://www.w3.org/TR/WCAG21/" target="_blank" rel="noopener noreferrer">Web Content Accessbility Guidelines (WCAG)</a> covers a wide range of recommendations for making Web content more accessible. Just a tiny part of making the web more accessible is accommodating for those with a form of blindness or low vision.</Text>
+          <Text>The standard grading system is a great start, but I thought I'd try to humanize the people who are affected by the different grades.</Text>
 
           <MediumText>Where did you get the info from?</MediumText>
-          <Text>The percentages are sourced from both <a href="www.colour-blindness.com" target="_blank" rel="noopener noreferrer">colour-blindness.com</a> and <a href="https://www.visionaustralia.org/" target="_blank" rel="noopener noreferrer">Vision Australia</a>.</Text>
+          <Text>The percentages are sourced from both <a href="www.colour-blindness.com" target="_blank" rel="noopener noreferrer">colour-blindness.com</a> and <a href="https://www.visionaustralia.org/" target="_blank" rel="noopener noreferrer">Vision Australia</a>. P.S. You're both the best, thankyou ✌️</Text>
 
           <MediumText>Your maths is off, it doesn't add up to 100%...?</MediumText>
-          <Text>Good eyes! (hehe) The percentages are there to give you an idea of how much of the population could be affected by the color combination. Many people will fall in to multiple vision types there's no way to accurately show perfect data. This is only just a fraction of the visual impairments out there as well.</Text>
+          <Text>Good eyes! (haha) The population data provided are estimates for individual impairments, and don't cover the vast amount of visual impairments in the world. This is to give you not just an understanding of <strong>how</strong> color contrast affects different people but also <strong>who</strong> it can affect.</Text>
 
-          <MediumText>How accurate is the data?</MediumText>
-          <Text>Vision impairments are never going to be the exact same for those with the same conditions, so the data provided is rough estimate. However the main objective is to give you a better understanding of how color contrast affects different vision types.</Text>
+          <MediumText>I'm fascinated by how this works, can you tell me more?</MediumText>
+          <Text>Of course! There's a few stages to get to this point. First we figure out the contrast between two HEX values. For this we're using a plugin called <a href="https://vis4.net/chromajs/" target="_blank" rel="noopener noreferrer">Chrome.JS</a> - this does the heavy lifting for us. Once we have the ratio (and using font size and font weight) we can apply a grade to that specific color combo.</Text>
+          <Text>For the color blindness options we're using another plugin aptly called <a href="https://github.com/skratchdot/color-blind" target="_blank" rel="noopener noreferrer">Color-blind</a> that converts our HEX codes in to ones that would be seen by people with the different impairments, then we can apply our same process to obtain the color ratios and determine their grade.</Text>
+          <Text>For cataracts, glaucoma, low vision, and the situational events I've personally created simulations to help identify their rating.</Text>
 
           <MediumText>What does a a failing grade mean?</MediumText>
-          <Text>A fail simply means that the color combination offers some visual strain to the person seeing it. You might still be able to read some of the different vision types, but your eyes are working overtime.</Text>
+          <Text>The grading uses a combination of color contrast, text size and text weight. A fail simply means that the color combination offers some visual strain to the person seeing it and should be avoided if possible.</Text>
 
-          <MediumText>Can I support this site?</MediumText>
-          <Text>Absolutely! You can either help contribute to the codebase or sponsor me on GitHub to help me keep whocanuse.com running.</Text>
+          <MediumText>Can I help contribute?</MediumText>
+          <Text>Absolutely! Feel free to fork the repo and submit a PR with any helpful additions or changes.</Text>
 
         </AboutWrapper>
         <Author>
-          <div>
-            <a class="github-button" href="https://github.com/sponsors/coreyginnivan" data-icon="octicon-heart" aria-label="Sponsor @coreyginnivan on GitHub">Sponsor</a>
-            &nbsp;
+          <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+            <VersionWrapper>v1.0.0</VersionWrapper>
             <a class="github-button" href="https://github.com/coreyginnivan/whocanuse/fork" data-icon="octicon-repo-forked" aria-label="Fork coreyginnivan/whocanuse on GitHub">Fork</a>
           </div>
           <Text>
