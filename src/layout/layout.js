401038899ba27857490f7b9a27918653f527ff81
Jake Ginnivan <jake.ginnivan@wanews.com.au>
2019-11-13 18:22:25 +0800

    Fix og:image path

--- src/layout/layout.js
+++ src/layout/layout.js
@@ -3,7 +3,7 @@ import Helmet from 'react-helmet'
 import { StaticQuery, graphql } from 'gatsby'
 
 import favicon from '../images/favicon.ico'
-import ogpimage from 'raw-loader!../images/whocanuse_600.png';
+import ogpimage from '!!file-loader!../images/whocanuse_600.png'
 
 import './layout.css'
 
@@ -23,12 +23,24 @@ const Layout = ({ children }) => (
         <Helmet>
           <title>{data.site.siteMetadata.title}</title>
           <meta name="Content-Type" content="text/html; charset=UTF-8" />
-          <meta name="description" content="Find out who can use your color combination" />
-          <meta name="keywords" content="accessibility, color, contrast, tool" />
+          <meta
+            name="description"
+            content="Find out who can use your color combination"
+          />
+          <meta
+            name="keywords"
+            content="accessibility, color, contrast, tool"
+          />
           <meta property="og:type" content="website" />
           <meta property="og:site_name" content="Who Can Use" />
-          <meta property="og:title" content="Find out who can use your color combination" />
-          <meta property="og:description" content="A tool that brings attention and understanding to how color contrast can affect different people with visual impairments." />
+          <meta
+            property="og:title"
+            content="Find out who can use your color combination"
+          />
+          <meta
+            property="og:description"
+            content="A tool that brings attention and understanding to how color contrast can affect different people with visual impairments."
+          />
           <meta property="og:image" content={ogpimage} />
           <meta property="og:url" content="https://www.whocanuse.com/" />
           <meta property="og:image:width" content="600" />
@@ -36,13 +48,22 @@ const Layout = ({ children }) => (
           <meta name="twitter:card" content="summary" />
           <meta name="twitter:site" content="@coreyginnivan" />
           <meta name="twitter:title" content="Who Can Use" />
-          <meta name="twitter:description" content="Find out who can use your color combination. Whocanuse is a tool that brings attention and understanding to how color contrast can affect different people that have visual impairments." />
+          <meta
+            name="twitter:description"
+            content="Find out who can use your color combination. Whocanuse is a tool that brings attention and understanding to how color contrast can affect different people that have visual impairments."
+          />
           <meta name="twitter:image" content={ogpimage} />
           <link rel="shortcut icon" type="image/png" href={favicon} />
-          <script async defer src="https://buttons.github.io/buttons.js"></script>
+          <script
+            async
+            defer
+            src="https://buttons.github.io/buttons.js"
+          ></script>
         </Helmet>
         <div>{children}</div>
       </React.Fragment>
-    )} />)
+    )}
+  />
+)
 
 export default Layout
