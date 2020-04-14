9c6d2bf725ab0d1af34ae4f10dc39995e97029ae
JakeGinnivan <jake@ginnivan.net>
2020-04-14 20:20:26 +0800

    Fixed url updating

--- src/helpers/link.js
+++ src/helpers/link.js
@@ -1,4 +1,3 @@
 export function linkPath(background, foreground, fontSize, bold) {
-  return `?b=${background}&c=${foreground}&f=${fontSize}
-  }${bold ? 'b' : ''}`
+  return `?b=${background}&c=${foreground}&f=${fontSize}&s=${bold ? 'b' : ''}`
 }
