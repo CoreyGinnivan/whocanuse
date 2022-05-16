4c3576e6a70185d26659a4d7443fb1142ccddee1
Corey Ginnivan <corey@ginnivan.net>
2022-05-16 22:34:28 +0800

    Updated SEO info

--- src/components/seo.js
+++ src/components/seo.js
@@ -7,17 +7,33 @@ export default function SEO({ description, title, image }) {
   return (
     <Head>
       <title>{`${title} | ${siteTitle}`}</title>
-      <meta name="description" content={description} />
+      <meta
+        name="description"
+        content="A tool that brings attention and understanding to how color contrast can affect people with different visual impairments."
+      />
       <meta property="og:type" content="website" />
-      <meta property="og:title" content={title} />
-      <meta property="og:image" content={image} />
-      <meta property="og:description" content={description} />
+      <meta property="og:url" content="https://whocanuse.com" />
+      <meta property="og:title" content="WhoCanUse" />
+      <meta
+        property="og:image"
+        content="https://whocanuse.com/whocanuse_banner.png"
+      />
+      <meta
+        property="og:description"
+        content="A tool that brings attention and understanding to how color contrast can affect people with different visual impairments."
+      />
       <meta property="og:site_name" content={siteTitle} />
       <meta property="twitter:card" content="summary_large_image" />
-      <meta property="twitter:image" content={image} />
-      <meta property="twitter:creator" content={config.twitter} />
-      <meta property="twitter:title" content={title} />
-      <meta property="twitter:description" content={description} />
+      <meta
+        property="twitter:image"
+        content="https://whocanuse.com/whocanuse_banner.png"
+      />
+      <meta property="twitter:creator" content="@CoreyGinnivan" />
+      <meta property="twitter:title" content="WhoCanUse" />
+      <meta
+        property="twitter:description"
+        content="A tool that brings attention and understanding to how color contrast can affect people with different visual impairments."
+      />
       <link rel="shortcut icon" href="/favicon.ico" />
     </Head>
   )
