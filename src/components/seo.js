c35df30890d2004dbe46e73305d51be1f5e7db9c
Corey Ginnivan <corey@ginnivan.net>
2022-05-16 22:22:56 +0800

    Added Twitter image reference

--- src/components/seo.js
+++ src/components/seo.js
@@ -14,6 +14,7 @@ export default function SEO({ description, title, image }) {
       <meta property="og:description" content={description} />
       <meta property="og:site_name" content={siteTitle} />
       <meta property="twitter:card" content="summary_large_image" />
+      <meta property="twitter:image" content={image} />
       <meta property="twitter:creator" content={config.twitter} />
       <meta property="twitter:title" content={title} />
       <meta property="twitter:description" content={description} />
